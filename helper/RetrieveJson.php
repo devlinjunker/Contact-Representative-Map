17416e56642e1d1bea5d071ff7965183e646b1b2
devlinjunker <devlin@osu>
2012-06-16 14:18:51 -0700

    php XML to JSON parser

---
+++ helper/RetrieveJson.php
@@ -0,0 +1,16 @@
+<?php
+	header("content-type: application/json charset:utf-8");
+	$url = $_GET['url'];
+	
+	if(strlen($url) >= 13 ) { 
+		$xml = file_get_contents(urldecode($_GET["url"])); 
+	
+		if($xml) { 
+			$data = @simplexml_load_string($xml, "SimpleXMLElement", LIBXML_NOCDATA); 
+		
+			$json = json_encode($data); 
+			echo isset($_GET["callback"]) ? "{$_GET[?callback?]}($json)" : 
+											$json; 
+		} 
+	} 
+?>
