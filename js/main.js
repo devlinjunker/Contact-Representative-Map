8b5ffecbb578f1f8f7238b34155c79f618d230e9
devlinjunker <devlin@osu>
2012-06-16 16:24:39 -0700

    sizing

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 function initialize() {
 	var myOptions = {
-	  center: new google.maps.LatLng(-34.397, 150.644),
-	  zoom: 8,
+	  center: new google.maps.LatLng(39.504041,-102.216797),
+	  zoom: 4,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	
-	var map = new google.maps.Map(document.getElementById("map_canvas"),
+	var map = new google.maps.Map(document.getElementById("continental"),
 									myOptions);
-		
+	
+	var small_map = new google.maps.Map(document.getElementById("hawaii"),
+									myOptions);
 	$.ajax({
 			type: 	"GET",
 			url: 	"helper/RetrieveXML.php?url=http://www.senate.gov/general/contact_information/senators_cfm.xml",
