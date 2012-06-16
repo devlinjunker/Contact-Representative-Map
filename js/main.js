864196bf4dcfa814122f1cbdecdb6bf4483f425b
devlinjunker <devlin@osu>
2012-06-16 13:33:57 -0700

    jsonp

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-	$.ajax({
-			type: 	"GET",
-			url: 	"http://www.senate.gov/general/contact_information/senators_cfm.xml",
-			datatype: "xml",
-			success:function(xml){
-						alert(xml);
-					}
-			});
-			
-			
-});
+function parseXML(xml){
+	alert(xml);
+}
